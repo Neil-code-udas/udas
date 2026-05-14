626791744dbd34971a7a6086e1af880174eadc45
cuiyinana <cuiyinan1998@163.com>
2026-05-15 02:20:59 +0800

    提交1

--- src/main/java/com/cyn/udas/UdasApplication.java
+++ src/main/java/com/cyn/udas/UdasApplication.java
@@ -1,9 +1,11 @@
 package com.cyn.udas;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cyn.udas.mapper")
 public class UdasApplication {
 
     public static void main(String[] args) {
